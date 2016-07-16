345dd0a4d8eca57795fc9a96ba9e654bba875390
leseulsteve <leseulsteve@gmail.com>
2016-07-16 11:28:21 -0400

    no message

--- app/modules/core/config/core.config.js
+++ app/modules/core/config/core.config.js
@@ -10,14 +10,18 @@ angular.module('gestigris-common').config(
         'request': function (config) {
           if (!_.endsWith(config.url, '.html') && !_.endsWith(config.url, '.json') &&  !_.startsWith(config.url, 'http')) {
             //var urlPrefix = 'http://vps54578.vps.ovh.ca:90',
-            var urlPrefix = 'http://localhost:9011',
+            var urlPrefix = 'http://localhost:9011';
 
-              route = config.url.split('/')[config.url.split('/').length - 2];
+            if (!_.startsWith(config.url, 'photon')) {
 
-            if (!_.includes(['img', 'icons', '/'], route) && !_.endsWith(config.url, 'md-close.svg') && !_.endsWith(config.url, 'md-tabs-arrow.svg')) {
-              urlPrefix += '/api/v1';
-              config.url = urlPrefix + '/' + config.url;
-            }
+                var route = config.url.split('/')[config.url.split('/').length - 2];
+
+                if (!_.includes(['img', 'icons', '/'], route) && !_.endsWith(config.url, 'md-close.svg') && !_.endsWith(config.url, 'md-tabs-arrow.svg')) {
+                  urlPrefix += '/api/v1';
+                  config.url = urlPrefix + '/' + config.url;
+                }
+
+              }
 
           }
           return config;
